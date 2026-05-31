e6330fe054f458396c7b1d9fbc54971b5a3b5e7d
Sourin De <sdgamer8263@gmail.com>
2026-05-31 23:53:41 +0530

    Update Plugin.sh

--- Plugin.sh
+++ Plugin.sh
@@ -1,5 +1,5 @@
 cd /var/www/pterodactyl && \
-curl -sL https://raw.githubusercontent.com/sdgamer8263-sketch/pterodactyl_extention1/main/trr/Xminecraft-plugin-manager-for-pterodactyl-v221.zip -o plugin_manager.zip && \
+curl -sL https://raw.githubusercontent.com/sdgamer8263-sketch/pterodactyl_extention1/main/trr/plugin-manager.zip -o plugin_manager.zip && \
 unzip -o plugin_manager.zip && \
 if [ -d "upload" ]; then cp -r upload/* ./ && rm -rf upload; fi && \
 rm plugin_manager.zip && \
