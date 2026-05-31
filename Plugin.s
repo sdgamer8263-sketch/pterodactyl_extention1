df7b971fb4ec8830ed2ef7eab02ab49ce2cc3b1f
Sourin De <sdgamer8263@gmail.com>
2026-05-31 23:55:26 +0530

    Update Plugin.s

--- Plugin.s
+++ Plugin.s
@@ -1,12 +1,11 @@
 cd /var/www/pterodactyl && \
-curl -sL https://raw.githubusercontent.com/sdgamer8263-sketch/pterodactyl_extention1/main/trr/plugin-manager.zip -o plugin_manager.zip && \
-unzip -o plugin_manager.zip && \
+curl -sL https://raw.githubusercontent.com/sdgamer8263-sketch/pterodactyl_extention1/main/trr/plugin.zip -o plugin.zip && \
+unzip -o plugin.zip && \
 if [ -d "upload" ]; then cp -r upload/* ./ && rm -rf upload; fi && \
-rm plugin_manager.zip && \
+rm plugin.zip && \
 npm install --global yarn && \
 yarn install && \
 export NODE_OPTIONS=--openssl-legacy-provider && \
 yarn run build:production && \
 php artisan migrate --force && \
 php artisan optimize
-
