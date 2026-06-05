1961dc6198b4f3d065d0c50b0f845cbc138b5ac5
Sourin De <sdgamer8263@gmail.com>
2026-06-05 20:18:06 +0530

    Update and rename arix-install.sh to arix-install.s

--- arix-install.s
+++ arix-install.s
@@ -75,6 +75,7 @@ echo -e "${CYAN}-> Running Arix installer command...${NC}"
 php artisan arix install
 
 echo -e "${CYAN}-> Building the Pterodactyl Panel (This might take a few minutes)...${NC}"
+yarn add xterm-addon-unicode11
 yarn build:production
 
 
