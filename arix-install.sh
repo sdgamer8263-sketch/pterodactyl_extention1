ec90389fc1927c8b22791a7281ffcdcec704d7e8
Sourin De <sdgamer8263@gmail.com>
2026-07-21 01:09:27 +0530

    Update arix-install.sh

--- arix-install.sh
+++ arix-install.sh
@@ -214,34 +214,172 @@ success "Files extracted successfully."
 step "3/4" "Injecting Arix Patch Modules..."
 cat << 'EOF' > app/Console/Commands/Arix.php
 <?php
-namespace App\Console\Commands;
+<?php
+
+namespace Pterodactyl\Console\Commands;
+
 use Illuminate\Console\Command;
+use Symfony\Component\Console\Formatter\OutputFormatterStyle;
 use Illuminate\Support\Facades\File;
 
 class Arix extends Command
 {
-    protected $signature = 'arix {action?}';
-    protected $description = 'Arix Theme Command';
-    public function handle() {
+    protected $signature = "arix {action?}";
+    protected $description = "All commands for Arix Theme for Pterodactyl.";
+
+    public function handle()
+    {
         $action = $this->argument("action");
-        if ($action === "install") $this->installOrUpdate();
-        elseif ($action === "update") $this->installOrUpdate(true);
+        $title = new OutputFormatterStyle("#fff", null, ["bold"]);
+        $this->output->getFormatter()->setStyle("title", $title);
+        $b = new OutputFormatterStyle(null, null, ["bold"]);
+        $this->output->getFormatter()->setStyle("b", $b);
+
+        if ($action === null) {
+            $this->line("\r\n            <title>\r\n            ░█████╗░██████╗░██╗██╗░░██╗\r\n            ██╔══██╗██╔══██╗██║╚██╗██╔╝\r\n            ███████║██████╔╝██║░╚███╔╝░\r\n            ██╔══██║██╔══██╗██║░██╔██╗░\r\n            ██║░░██║██║░░██║██║██╔╝╚██╗\r\n            ╚═╝░░╚═╝╚═╝░░╚═╝╚═╝╚═╝░░╚═╝\r\n\r\n           Thank you for purchasing Arix</title>\r\n\r\n           > php artisan arix (this window)\r\n           > php artisan arix install\r\n           > php artisan arix update\r\n           > php artisan arix uninstall\r\n            ");
+        } else {
+            $this->info("\n    Arix Theme\n    \n");
+            if ($action === "install") {
+                $this->install();
+            } elseif ($action === "update") {
+                $this->update();
+            } elseif ($action === "uninstall") {
+                $this->uninstall();
+            } else {
+                $this->error("Invalid action. Supported actions: install, update, uninstall");
+            }
+        }
     }
-    public function installOrUpdate($isUpdate = false) {
+
+    public function installOrUpdate($isUpdate = false)
+    {
+        if ($isUpdate) {
+            $this->info("\n    This command is not recommended to use. \n   This command skips frequently used files by addons during theme updating to avoid losing your addon customizations.\n   If you still experience an error after updating please contact us.");
+        }
+
+        $confirmation = $this->confirm("Are all the required dependencies installed from the readme file?", "yes");
+        if (!$confirmation) {
+            return;
+        }
+
         $versions = File::directories("./arix");
-        $version = $this->choice("Select a version:", array_map('basename', $versions));
-        exec("rsync -a arix/{$version}/ ./");
+        if (empty($versions)) {
+            $this->info("No versions found in /arix directory.");
+            return;
+        }
+
+        $version = basename($this->choice("Select a version:", $versions));
+        $this->info("Installing Arix Theme {$version}...");
+
+        $excludeOption = $isUpdate ? "--exclude='routes.ts' --exclude='getServer.ts' --exclude='admin.blade.php' --exclude='admin.php' --exclude='ServerTransformer.php'" : '';
+        exec("rsync -a {$excludeOption} arix/{$version}/ ./");
+
+        $directoryPath = app_path("Http/Controllers/Admin/Arix");
+        File::makeDirectory($directoryPath, 0755, true, true);
+
+        $filesOne = ["ArixController", "ArixAdvancedController", "ArixAnnouncementController", "ArixColorsController", "ArixComponentsController", "ArixDashboardController", "ArixLayoutController"];
+        $this->info("Proceeding with the installation...");
+        foreach ($filesOne as $file) {
+            $this->aa($file, $version, $directoryPath);
+            sleep(1);
+        }
+
+        $filesTwo = ["ArixLinkController", "ArixMailController", "ArixMetaController", "ArixPresetController", "ArixSocialController", "ArixStylingController"];
+        foreach ($filesTwo as $file) {
+            $this->aa($file, $version, $directoryPath);
+            sleep(1);
+        }
+
+        $this->info("Migrating database...");
         $this->command("php artisan migrate --force");
+
+        $this->info("Installing required packages...");
+        $this->info("This can take a minute...");
         $this->command("yarn add react-email-editor react-colorful recharts@^2.15.4 ua-parser-js cronstrue react-day-picker jszip react-turnstile @dnd-kit/core @dnd-kit/sortable @dnd-kit/utilities @types/md5 md5 react-icons@5.4.0 markdown-to-jsx@7.7.10 i18next-browser-languagedetector@7.2.1");
+
+        $this->info("Compile translations...");
         $this->command("php artisan language:compile");
+
+        $this->info("Building panel assets...");
+        $this->info("This can take a minute...");
+        $nodeVersion = shell_exec("node -v");
+        $nodeVersion = (int) ltrim($nodeVersion, "v");
+        if ($nodeVersion >= 17) {
+            $this->info("Node.js version is v" . $nodeVersion . " (>= 17)");
+            putenv("NODE_OPTIONS=--openssl-legacy-provider");
+        } else {
+            $this->info("Node.js version is v" . $nodeVersion . " (< 17)");
+        }
         $this->command("yarn build:production");
-        $this->command("chown -R www-data:www-data /var/www/pterodactyl/* " . base_path() . "/");
+
+        $this->info("Set permissions...");
+        $this->command("chown -R www-data:www-data /var/www/pterodactyl/* " . base_path() . "/*");
+        $this->command("chown -R nginx:nginx " . base_path() . "/*");
+        $this->command("chown -R apache:apache " . base_path() . "/*");
+
+        $this->info("Optimize application...");
         $this->command("php artisan optimize:clear");
         $this->command("php artisan optimize");
+
+        $this->info("Restarting workers...");
         $this->command("php artisan queue:restart");
-        $this->info("Theme installed successfully");
+
+        $message = $isUpdate ? "│    Theme updated successfully   │" : "│   Theme installed successfully  │";
+        $this->line("\n ╭───────────────────────────────╮\n │ │\n │ ╭─╴ {$message} ╶─╮ │\n │ ╰─╴ successfully ╶─╯ │\n │ │\n ╰───────────────────────────────╯\n ");
     }
-    private function command($cmd) { return exec($cmd); }
+
+    private function aa($filename, $version, $directoryPath)
+    {
+        $filePath = $directoryPath . "/" . $filename . ".php";
+        $localSource = base_path("arix/" . $version . "/app/Http/Controllers/Admin/Arix/" . $filename . ".php");
+
+        if (File::exists($localSource)) {
+            $this->info(" -> Copying local {$filename}.php...");
+            File::copy($localSource, $filePath);
+        } else {
+            $this->error("Fail: Could not find local {$filename}.php at {$localSource}.");
+        }
+    }
+    
+    public function install()
+    {
+        $this->info("Configuring environment...");
+        sleep(1);
+        $this->info("Loading modules...");
+        sleep(1);
+        
+        $this->installOrUpdate();
+        
+        $this->info("Arix Theme installation completed safely!");
+    }
+
+    public function update()
+    {
+        $this->installOrUpdate(true);
+    }
+
+    private function uninstall()
+    {
+        $this->line("Uninstalling...");
+        $this->command("php artisan down");
+        $this->command("curl -L https://github.com/pterodactyl/panel/releases/latest/download/panel.tar.gz | tar -xzv");
+        $this->command("chmod -R 755 storage/* bootstrap/cache");
+        $this->command("composer install --no-dev --optimize-autoloader");
+        $this->command("php artisan view:clear");
+        $this->command("php artisan config:clear");
+        $this->command("php artisan migrate --seed --force");
+        $this->command("chown -R www-data:www-data " . base_path() . "/*");
+        $this->command("chown -R nginx:nginx " . base_path() . "/*");
+        $this->command("chown -R apache:apache " . base_path() . "/*");
+        $this->command("php artisan queue:restart");
+        $this->command("php artisan up");
+        $this->info("Arix Theme uninstalled successfully.");
+    }
+
+    private function command($cmd)
+    {
+        return exec($cmd);
+    }
 }
 EOF
 php artisan arix install
