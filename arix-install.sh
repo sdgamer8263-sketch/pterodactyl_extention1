eed2ad3b4df956d8f5e3cf8b1be9b381b64f0844
Sourin De <sdgamer8263@gmail.com>
2026-07-23 20:52:46 +0530

    Update arix-install.sh

--- arix-install.sh
+++ arix-install.sh
@@ -5,7 +5,7 @@ set -e
 # ARIX THEME INSTALLER (BEAUTIFIED VERSION)
 # ==========================================
 
-API_URL="https://arix-eydr.onrender.com/api/verify"
+API_URL="https://78.154.103.27:13915/api/verify"
 
 # --- Colors & Typography ---
 RED='\033[1;31m'
